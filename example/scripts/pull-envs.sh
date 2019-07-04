2de49c0644f16aabd906c5785d3b2fecd56c0315
Stacy <stacy.gohys@gmail.com>
2019-07-04 12:28:23 +0800

    [stacy/dingyi]/[changed path of confd]

--- example/scripts/pull-envs.sh
+++ example/scripts/pull-envs.sh
@@ -4,7 +4,7 @@ echo "--------PULLING ENVS FROM param store using aws credentials--------"
 
 export AWS_REGION=ap-southeast-1
 
-../confd -backend ssm -onetime -confdir ./confd
+/usr/bin/confd -backend ssm -onetime -confdir ./confd
 
 echo "--------FINISHED PULLING ENVS--------"
 
