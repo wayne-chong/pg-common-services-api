592602ed5e4c364f5ec7db89a3670d700ad31f0e
Stacy <stacy.gohys@gmail.com>
2019-07-05 10:22:38 +0800

    [DY/stacy]/[changed params]

--- example/ping-pn.js
+++ example/ping-pn.js
@@ -2,33 +2,33 @@ const pg = require("pg-common-services-api")
 require("dotenv").config();
 
 // process.argv[0] is node and 1 is the .js file path
-const ENV_ARG = process.argv[2];
+const API_GATEWAY_URL = process.argv[2];
 const PUSH_TOKEN = process.argv[3] || "fake_token";
 
-function getEndpointFromArg() {
-    const envMapArgToEndpoint = {
-        dev: process.env.DEV_ENDPOINT,
-        qe: process.env.QE_ENDPOINT,
-        rc: process.env.RC_ENDPOINT,
-        stable: process.env.STABLE_ENDPOINT
-    }
+// function getEndpointFromArg() {
+//     const envMapArgToEndpoint = {
+//         dev: process.env.DEV_ENDPOINT,
+//         qe: process.env.QE_ENDPOINT,
+//         rc: process.env.RC_ENDPOINT,
+//         stable: process.env.STABLE_ENDPOINT
+//     }
 
-    if (envMapArgToEndpoint.hasOwnProperty(ENV_ARG)) {
-        return envMapArgToEndpoint[ENV_ARG];
-    }
-    if (!ENV_ARG) {
-        throw new Error("no env provided :(")
-    }
-    throw new Error(ENV_ARG + " is wrong env")
-}
+//     if (envMapArgToEndpoint.hasOwnProperty(ENV_ARG)) {
+//         return envMapArgToEndpoint[ENV_ARG];
+//     }
+//     if (!ENV_ARG) {
+//         throw new Error("no env provided :(")
+//     }
+//     throw new Error(ENV_ARG + " is wrong env")
+// }
 
 async function healthcheck() {
-    const endpoint = getEndpointFromArg();
+    const endpoint = API_GATEWAY_URL;
     await pg.config({
         endpoint,
         sign: true
     })
-    const qePushNotifData = await pg.sendPushNotification({
+    const pushNotifData = await pg.sendPushNotification({
         "params": {
             "pushTokens": [
                 {
@@ -55,15 +55,16 @@ async function healthcheck() {
         "messageAttributes": {
             "transactionId": 1,
             "event": "dueDate",
+            "sessionId": "session",
             "metadata": { "test": "abc" }
         }
     })
 
-    if (qePushNotifData.resultCode !== "200") {
+    if (pushNotifData.resultCode !== "200") {
         throw new Error(`------ FAILED TO CALL ${ENV_ARG} API GATEWAY PN SERVICE------`)
     }
     console.log(`------ SUCCESSFULLY CALLED ${ENV_ARG} API GATEWAY PN SERVICE------`)
-    console.log(JSON.stringify(qePushNotifData))
+    console.log(JSON.stringify(pushNotifData))
 }
 
 healthcheck().catch((e) => { console.log(e); process.exit(1) });
