69eef4680fe64d2484410e0c2e2fb65f75d64ec1
Dingyi <dyihoon90@gmail.com>
2019-07-31 10:23:13 +0800

    [PG-727][DY] install confd in bin to pull env vars for load test

--- example/scripts/pull-envs.sh
+++ example/scripts/pull-envs.sh
@@ -1,10 +1,19 @@
 #/bin/sh
 
-echo "--------PULLING ENVS FROM param store using aws credentials--------"
+echo "--------Installing confd in example/bin--------"
+if [ ! -x ./bin/confd ]; then
+    mkdir ./bin/
+    wget https://github.com/kelseyhightower/confd/releases/download/v0.16.0/confd-0.16.0-darwin-amd64
+    mv confd-0.16.0-darwin-amd64 ./bin/confd
+fi
+
+chmod +x ./bin/confd
+
+echo "--------Pulling env variables from param store using aws credentials--------"
 
 export AWS_REGION=ap-southeast-1
 
-/usr/bin/confd -backend ssm -onetime -confdir ./confd
+./bin/confd -backend ssm -onetime -confdir ./confd
 
-echo "--------FINISHED PULLING ENVS--------"
+echo "--------Finished pulling env variables--------"
 
