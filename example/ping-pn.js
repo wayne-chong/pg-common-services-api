6981b5e9650de1b51ab80e38f9475abd4affa689
Dingyi <dyihoon90@gmail.com>
2020-03-06 17:29:43 +0800

    [MISC][DY]Update example

--- example/ping-pn.js
+++ example/ping-pn.js
@@ -1,4 +1,4 @@
-const pg = require("pg-common-services-api")
+const pg = require("pg-common-services-api");
 require("dotenv").config();
 
 // process.argv[0] is node and 1 is the .js file path
@@ -10,53 +10,59 @@ const PRIVATE = process.argv[6] || false;
 const HOST = process.argv[7] || null;
 
 async function healthcheck() {
-    const endpoint = API_GATEWAY_URL;
-    await pg.config({
-        endpoint,
-        sign: true,
-        private: PRIVATE,
-        stage: STAGE,
-        host: HOST,
-        credentialProvider: CREDENTIAL_PROVIDER
-    })
-    const pushNotifData = await pg.sendPushNotification({
-        "params": {
-            "pushTokens": [
-                {
-                    "parentId": "fakeId",
-                    "pushToken": PUSH_TOKEN,
-                    "deviceOS": "android",
-                    "deviceModel": "Android Galaxy 8"
-                },
-            ],
-            "pushPayload": {
-                "notification": {
-                    "title": "Test Annoucement",
-                    "body": "PN common service",
-                    "android_channel_id": "pg-channel",
-                    "priority": "high",
-                    "badge": "0"
-                },
-                "data": {
-                    "type": "new_announcement",
-                    "payload": "1"
-                }
-            }
-        },
-        "messageAttributes": {
-            "transactionId": 1,
-            "event": "dueDate",
-            "sessionId": "session",
-            "metadata": { "test": "abc" }
+  const endpoint = API_GATEWAY_URL;
+  await pg.config({
+    endpoint,
+    sign: true,
+    private: PRIVATE,
+    stage: STAGE,
+    host: HOST,
+    credentialProvider: CREDENTIAL_PROVIDER
+  });
+  const pushNotifData = await pg.sendPushNotification({
+    params: {
+      pushTokens: [
+        {
+          parentId: "fakeId",
+          pushToken: PUSH_TOKEN,
+          deviceOS: "android",
+          deviceModel: "Android Galaxy 8"
         }
-    })
-
-
-    if (pushNotifData.resultCode !== "200") {
-        throw new Error(`------ FAILED TO CALL API GATEWAY PN SERVICE------`, pushNotifData)
+      ],
+      pushPayload: {
+        notification: {
+          title: "Test Annoucement",
+          body: "PN common service",
+          android_channel_id: "pg-channel",
+          priority: "high",
+          badge: "0"
+        },
+        data: {
+          type: "new_announcement",
+          payload: "1"
+        }
+      }
+    },
+    messageAttributes: {
+      transactionId: 1,
+      event: "dueDate",
+      sessionId: "session",
+      metadata: { test: "abc" }
     }
-    console.log(`------ SUCCESSFULLY CALLED API GATEWAY PN SERVICE------`)
-    console.log(JSON.stringify(pushNotifData))
+  });
+
+  console.log("returned data is: ");
+  console.log(JSON.stringify(pushNotifData));
+  if (pushNotifData.resultCode !== "200") {
+    throw new Error(
+      `------ FAILED TO CALL API GATEWAY PN SERVICE------`,
+      pushNotifData
+    );
+  }
+  console.log(`------ SUCCESSFULLY CALLED API GATEWAY PN SERVICE------`);
 }
 
-healthcheck().catch((e) => { console.log(e); process.exit(1) });
+healthcheck().catch(e => {
+  console.log(e);
+  process.exit(1);
+});
