0bf0458a6315f93bf11cc054cdb1926306a0df2d
Dingyi <dyihoon90@gmail.com>
2020-03-13 14:55:23 +0800

    [MISC][DY|JT]Prepare repo to publish to npm repo - publishing to the @pgateway org in npm repo as a public library

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
 import { createBaseLibConfig } from "./webpack/lib.webpack.config";
 
 const webPackConfig = createBaseLibConfig({
-    name: "pg-common-services-api",
-    dir: "./",
+  name: "@pgateway/common-services-api",
+  dir: "./",
 });
 
 export default webPackConfig;
